fe0e27ff32ac64e964870c3c45ad003b3c0f5f69
Tomás Fedora <moronosoutotomas.sangrina@gmail.com>
2025-03-14 21:09:29 +0100

    - saludo dinámico con Astro islands

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://tomasmorono-astro-project.netlify.app/',
+  site: 'https://tomasmorono-astro-project.netlify.app/',
+  integrations: [preact()],
 });
